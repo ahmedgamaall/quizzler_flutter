bd98789e9b85cb0e65f4789c1886a52eac2fac3f
ahmedgamaall <ahmedgamalahmed231@gmail.com>
2022-10-24 01:13:37 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Quizzler Challenge Solution
---
+++ android/app/src/main/kotlin/com/example/quizzler_flutter_challenge_final/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.quizzler_flutter_challenge_final
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
